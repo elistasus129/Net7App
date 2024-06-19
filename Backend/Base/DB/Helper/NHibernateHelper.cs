f814bf97950db650b755f1d192304e8a131ba3f8
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-06-20 00:23:06 +0300

    ISessionFactory

--- Backend/Base/DB/Helper/NHibernateHelper.cs
+++ Backend/Base/DB/Helper/NHibernateHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NHibernate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,20 @@ namespace Base.DB.Helper
 {
     public abstract class NHibernateHelper : IDisposable
     {
+
+        private static ISessionFactory _sessionFactory;
+
+        public virtual ISessionFactory SessionFactory {
+           get {  return _sessionFactory ?? (_sessionFactory = InitializeFactory()); } 
+        
+        }
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
         public void Dispose()
         {
             GC.SuppressFinalize(this);
