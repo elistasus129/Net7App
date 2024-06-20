db05a44d5d70f6c0ce0779b3049dd93cfc7585d2
Mert Ramazanoglu <61631884+elistasus129@users.noreply.github.com>
2024-06-21 00:54:01 +0300

    repository kodlaması

--- Backend/Base/DB/Interface/IRepositoryService.cs
+++ Backend/Base/DB/Interface/IRepositoryService.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Base.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Base.DB.Interface
 {
-    public interface IRepositoryService
+    public interface IRepositoryService<T> where T:class,IEntity,new()
     {
+        List<T> GetList(Expression<Func<T,bool>> filter=null);
+        
+        T Get(Expression<Func<T, bool>> filter = null);
+
+        T Add(T entity);    
+        T Update(T entity); 
+        void Delete(T entity);
     }
 }
